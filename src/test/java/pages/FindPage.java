2a3998c615b9fafbdaac9ab3eba7052556d3c919
artem <poi.ru0130@gmail.com>
2022-02-05 01:03:41 +0300

    add new Test

--- src/test/java/pages/FindPage.java
+++ src/test/java/pages/FindPage.java
@@ -7,13 +7,15 @@ import static com.codeborne.selenide.Selenide.$x;
 
 public class FindPage {
     SelenideElement
-            cookieAcceptButton = $x("//a[.='Использовать все cookies']");
+            cookieAcceptButton = $x("//a[.='Использовать все cookies']"),
+            films = $x("(//div[@class='VideoUnit_video_unit_content_cell_title__6hv5g'])[1]");
+
 
 
     public FindPage foundedFilms(String name){
         cookieAcceptButton.click();
-        SelenideElement
-                films = $x("(//*[contains(text(), '"+name+"')])[2]");
+//        SelenideElement
+//                films = $x("(//*[contains(text(), '"+name+"')])[2]");
         Assertions.assertTrue(films.getText().contains(name));
 
         films.click();
