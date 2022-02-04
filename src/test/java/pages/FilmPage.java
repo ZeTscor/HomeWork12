475c65cfcd64d88f364b3ddaf24f3e0546e2a12c
artem <poi.ru0130@gmail.com>
2022-02-04 22:55:45 +0300

    add new Test

--- src/test/java/pages/FilmPage.java
+++ src/test/java/pages/FilmPage.java
@@ -10,7 +10,7 @@ public class FilmPage {
     private SelenideElement
             ageAccept = $x("//div[@class='popups_check-age__button-agree__23TZ2'][.='Да']"),
             freeView = $x("//div[@class='TopBlock_TopBlock__button-play__28Kv- '][.= 'Смотреть бесплатно']"),
-            trellis = $x("//div[@class='Trailers_product__trailers-item-title__34jyB'][.= 'Трейлер']"),
+            trellis = $x("//h2[@class='Product_product__linked__2HnP4'][.='Трейлеры']"),
             castInfo = $x("//h2[@class='Product_product__linked__2HnP4 '][.= 'Актеры и съемочная группа']");
 
     public FilmPage openPage(){
