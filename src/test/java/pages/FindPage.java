a1600ab6787bc5cd243c60bfe6f952899fc6d57a
artem <poi.ru0130@gmail.com>
2022-02-05 00:33:47 +0300

    add new Test

--- src/test/java/pages/FindPage.java
+++ src/test/java/pages/FindPage.java
@@ -6,10 +6,11 @@ import org.junit.jupiter.api.Assertions;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class FindPage {
-    SelenideElement
-            films = $x("//div[@class='VideoUnit_video_unit_content_cell_title__6hv5g']");
+
 
     public FindPage foundedFilms(String name){
+        SelenideElement
+                films = $x(("//*[contains(text(), '"+name+")])[2]"));
         Assertions.assertTrue(films.getText().contains(name));
         films.scrollIntoView(true).click();
         return this;
